bb4ee60d1e04146a87e7a2a5997370f33ccee62b
Mario <canalessanchezmario@gmail.com>
2023-09-09 11:52:01 +0200

    Agregar archivos de proyecto.

---
+++ ClientApp/src/components/RankingItems.js
@@ -0,0 +1,24 @@
+﻿import React, { useEffect, useState } from 'react'
+
+const RankingItems = () => {
+    const [items, setItems] = useState([])
+    const dataType = 1
+
+    useEffect(() => {
+        fetch(`item/${dataType}`)
+            .then(res => {
+                return res.json()
+            })
+            .then(data => {
+                setItems(data)
+            })
+    }, [])
+
+    return (
+        <main>
+            {items !== null ? items.map(item => <h3>{item.title}</h3>):<div>Loading...</div>}
+        </main>
+    )
+}
+
+export default RankingItems
